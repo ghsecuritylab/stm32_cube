3fd516b6ba1f988cd9c19afe4a9a3480f31bb9b2
xiaofei <alibaba22001@163.com>
2019-10-11 17:42:48 +0800

    update

--- apps/test_pwm.c
+++ apps/test_pwm.c
@@ -6,7 +6,7 @@
  *   文件名称：test_pwm.c
  *   创 建 者：肖飞
  *   创建日期：2019年10月11日 星期五 16时26分47秒
- *   修改日期：2019年10月11日 星期五 16时59分22秒
+ *   修改日期：2019年10月11日 星期五 17时42分19秒
  *   描    述：
  *
  *================================================================*/
@@ -28,13 +28,13 @@ void test_pwm(void const *argument)
 		while (dutyCycle < 1000) {
 			dutyCycle++;
 			__HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_2, dutyCycle);
-			HAL_Delay(1);
+			osDelay(1);
 		}
 
 		while (dutyCycle) {
 			dutyCycle--;
 			__HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_2, dutyCycle);
-			HAL_Delay(1);
+			osDelay(1);
 		}
 
 		osDelay(200);
